bae3c296713aeaf0ae176840ae7aa42e91510edc
hxxtae <fkdlxmfkdl@gmail.com>
2022-09-05 21:58:44 +0900

    :book: [feat]: Add index of components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Containers } from './Containers';
+export { default as Loadings } from './Loadings';
+export { default as MainLoading } from './MainLoading';
+export { default as NotFound } from './Notfound';
