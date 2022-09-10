1b3e5011f745f7da98840604c5e589421ef9ea5b
hxxtae <fkdlxmfkdl@gmail.com>
2022-09-10 23:57:42 +0900

    :books: [Docs]: NOTE

--- src/hooks/querys/useQueriesMutating.ts
+++ src/hooks/querys/useQueriesMutating.ts
@@ -1,4 +1,4 @@
-import { useIsMutating } from 'react-query';
+import { useIsMutating, useQueryClient } from 'react-query';
 
 interface IUserQueriesMutating {
   isLoading: boolean;
@@ -10,3 +10,10 @@ export const useQueriesMutating = (): IUserQueriesMutating => {
 
   return { isLoading };
 }
+
+// NOTE: useIsFetching & useIsMutating 내부
+// ->
+// useIsFetching과 useIsMutating 내부에는 useEffect와 useState가 선언되어져 있다.
+// 만약 부모 컴포넌트에서 해당 훅을 사용한다면 fetching과 mutate가 발생시
+// 부모 컴포넌트가 리렌더링 되므로 사용시 주의해야 한다.
+// ref : https://tech.kakao.com/2022/06/13/react-query/
