abea860c5aaedf85248e1159b54e3f3ab274784f
hxxtae <fkdlxmfkdl@gmail.com>
2022-09-14 17:18:50 +0900

    :recycle: [refactory]: Implement CollectionItem
    Implement component of CollectionItem.tsx in CollectionList.tsx

--- src/pages/collections/page/CollectionBody/CollectionList/CollectionList.tsx
+++ src/pages/collections/page/CollectionBody/CollectionList/CollectionList.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 import { memo } from 'react';
 
-import { colors, thisIdExcept, media, noDrag, scroll } from '../../../../../utils';
+import { colors, noDrag, scroll } from '../../../../../utils';
 import { dto } from '../../../application/dto';
-import InputCheck from './InputCheck';
+import CollectionItem from './CollectionItem';
 
 interface ICollectionList {
   isLoading: boolean;
@@ -20,18 +20,8 @@ function CollectionList({ isLoading, collections, onDelete }: ICollectionList) {
         isLoading ?
           <Loading>Loading...</Loading> :
           collections?.map((item) => (
-            <li key={item.name}>
-              {thisIdExcept(item.name) ?
-                <NotCheck /> :
-                <InputCheck thisName={item.name} />}
-              <Wrapper>
-                <Name>{item.name}</Name>
-                <Type>{item.type}</Type>
-              </Wrapper>
-              {thisIdExcept(item.name) ||
-                <Delete type="button" onClick={() => onDelete(item.name)}>Delete</Delete>}
-            </li>  
-        ))
+            <CollectionItem key={item.name} collection={item} onDelete={onDelete} />
+          ))
       }
     </Contain>
   )
@@ -55,30 +45,8 @@ const Contain = styled.ul`
   ${scroll.bar(10)}
   ${scroll.track()}
   ${scroll.thumb(colors.secondary)}
-  
-  li {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    border-bottom: 1px solid ${colors.bgColor};
-    padding: 20px 0;
 
-    &:first-child {
-      padding-top: 0;
-    }
-
-    &:last-child {
-      border-bottom: none;
-      padding-bottom: 0;
-    }
-
-    ${noDrag()}
-  }
-
-  @media ${media.tablet_s} {
-    padding-left: 30px;
-    padding-right: 30px;
-  }
+  ${noDrag()}
 `;
 
 const Loading = styled.div`
@@ -86,52 +54,4 @@ const Loading = styled.div`
   font-size: 20px;
   color: ${colors.light};
   padding: 50px;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-`;
-
-const Name = styled.span`
-  font-size: 20px;
-  font-weight: 600;
-  padding: 10px 0;
-`;
-
-const Type = styled.span`
-  font-size: 16px;
-  color: ${colors.secondary};
-`;
-
-const Delete = styled.button`
-  font-size: 16px;
-  color: ${colors.light};
-  background-color: ${colors.red};
-  border-radius: 5px;
-  padding: 5px;
-  margin-left: auto;
-  cursor: pointer;
-  transition: opacity 200ms ease-in-out;
-
-  &:active {
-    opacity: .4;
-  }
-
-  @media ${media.tablet_s} {
-    &:hover {
-    opacity: .4;
-  }
-}
-`;
-
-const NotCheck = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: ${colors.red};
-  border-radius: 50%;
-  margin: 10px;
-  align-self: flex-start;
 `;
