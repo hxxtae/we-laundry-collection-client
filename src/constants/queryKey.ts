36e651062c0ddd8cce9a28517a1586e0092f434c
hxxtae <fkdlxmfkdl@gmail.com>
2023-08-07 22:22:24 +0900

    :books: [Docs]: Update queryKey.ts
    - Add const object of query key

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -1,24 +1,32 @@
+const BASE_PATH_KEYS = {
+  login: 'login' as const,
+  logout: 'logout' as const,
+  auth: 'auth' as const,
+  collections: 'collections' as const,
+  users: 'users' as const,
+}
+
 export const queryKey = {
   collect: {
-    all: ['/collections'] as const,
+    all: [`${BASE_PATH_KEYS.collections}`] as const,
     listName: (name: string) => [...queryKey.collect.all, `${name}`] as const,
   },
   auth: {
-    me: ['/auth/me'] as const,
+    me: [`/${BASE_PATH_KEYS.auth}/me`] as const,
   },
   user: {
-    all: ['/users'] as const,
+    all: [`/${BASE_PATH_KEYS.users}`] as const,
     listName: (name: string) => [...queryKey.user.all, `${name}`] as const,
   },
 };
 
 export const mutateKey = {
   collect: {
-    all: ['/collections'] as const,
+    all: [`/${BASE_PATH_KEYS.collections}`] as const,
   },
   user: {
-    all: ['/users'] as const,
+    all: [`/${BASE_PATH_KEYS.users}`] as const,
   },
-  login: ['/login'] as const,
-  logout: ['/logout'] as const,
+  login: [`/${BASE_PATH_KEYS.login}`] as const,
+  logout: [`/${BASE_PATH_KEYS.logout}`] as const,
 }
