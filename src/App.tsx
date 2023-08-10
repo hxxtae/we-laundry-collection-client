4a500bb2792974a1f645812ad97664bdf357d7b8
hxxtae <fkdlxmfkdl@gmail.com>
2023-08-10 13:01:05 +0900

    :bookmark: [Rename]: Move Query hook (login)
    - Add App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useRecoilValue } from 'recoil';
 
 import { LoginRouter, MainRouter } from './router';
 import { managerAuth } from './pages/login/application/context/login';
-import { useMe } from './pages/login/application/custom_hooks';
+import { useMe } from './hooks/querys/login';
 
 function App() {
   const manager = useRecoilValue(managerAuth);
