9ab4be3babcb11db1a9cceb68d11923041fb5c48
hxxtae <fkdlxmfkdl@gmail.com>
2023-08-25 22:53:01 +0900

    :book: [feat]: Add usersAPI file
    - Add api service file in service directory (users)

---
+++ src/service/usersAPI.ts
@@ -0,0 +1,26 @@
+import { api } from './api';
+import * as dto from '../types/users';
+
+/**
+ * 📗 API Function : User 목록 조회
+ * @returns {Promise<dto.IUser[]>}
+ */
+export const selectDatas = async (): Promise<dto.IUser[]> => {
+  const data = await api('/users', {
+    method: 'GET',
+  });
+  return data;
+}
+
+/**
+ * 📗 API Function : User 목록 삭제(단건)
+ * @param {string} username - User 이름
+ * @returns {Promise<boolean>}
+ */
+export const deleteData = async (username: string): Promise<boolean> => {
+  const data = await api(`/users/${username}`, {
+    method: 'DELETE',
+  });
+  return data;
+};
+  
