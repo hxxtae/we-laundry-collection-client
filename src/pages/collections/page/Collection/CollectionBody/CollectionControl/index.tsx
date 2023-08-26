c483db051c4eac25e765d7c8a56fdbd827af56b3
hxxtae <fkdlxmfkdl@gmail.com>
2023-08-26 20:09:14 +0900

    :books: [Docs]: Update CollectionBody.tsx
    - Add useForm props (defaultValues)
    - Update function (onDelete & onDeletes)
    - Update component props name (totla -> totalCount)
      - with. CollectionControl.tsx

--- src/pages/collections/page/Collection/CollectionBody/CollectionControl/index.tsx
+++ src/pages/collections/page/Collection/CollectionBody/CollectionControl/index.tsx
@@ -3,24 +3,24 @@ import { memo } from 'react';
 import * as S from './style';
 
 interface ICollectionControl {
-  total: number;
+  totalCount: number;
   checkCount: number;
   onReset: () => void;
 }
 
-function CollectionControl({ total, checkCount, onReset }: ICollectionControl) {
+function CollectionControl({ totalCount, checkCount, onReset }: ICollectionControl) {
   console.log("CollectionControl");
 
   return (
     <S.Contain>
       <S.Title>
         Collections
-        <S.Count>{ `Total: ${total}` }</S.Count>
+        <S.Count>{ `Total: ${totalCount}` }</S.Count>
       </S.Title>
       <S.Control>
         <S.Reset type="button" onClick={onReset}>Reset</S.Reset>
         <S.Deletes type="submit" chk={ checkCount }>
-          <img src="./assets/svg/trash-can-solid.svg" alt="delete all button of collection" />
+          <img src="./assets/svg/trash-can-solid.svg" alt="delete all button of collections" />
           <S.CheckNum>{ checkCount }</S.CheckNum>
         </S.Deletes>
       </S.Control>
--- src/pages/collections/page/Collection/CollectionBody/index.tsx
+++ src/pages/collections/page/Collection/CollectionBody/index.tsx
@@ -13,38 +13,35 @@ function CollectionBody() {
   const { delMutating, delData } = useCollectionDel();
   const { delsMutating, delDatas } = useCollectionsDel();
   const isLoading = isCollectLoading || isCollectFetching;
-  const method = useForm<dto.CollectionNamesDTO>();
-  const checkCount = method.watch().names ? method.watch().names.length : 0;
+  const method = useForm<dto.CollectionNamesDTO>({defaultValues: { names: [] }});
+  const checkCount = method.watch().names?.length || 0;
 
   const onDelete = useCallback((name: string) => {
-    const data = name;
-    const exceptName = thisIdExcept(data);
+    const exceptName = thisIdExcept(name);
     if (exceptName) {
-      alert('It cannot be done.');
+      alert(`It cannot be done. [ ${exceptName} ]`);
       return;
     }
-    const check = window.confirm(`Delete the [ ${data} ] collection?`);
+    const check = window.confirm(`Delete the [ ${name} ] collection?`);
     if (!check) return;
 
-    delMutating || delData(data, {
+    delMutating || delData(name, {
       onSuccess: () => method.reset(),
     });
   }, [delMutating, delData, method]);
 
   const onDeletes = ({ names }: dto.CollectionNamesDTO) => {
-    const datas = names;
-    if (!datas || !datas.length) {
-      return;
-    }
-    const exceptName = datas.find((name) => thisIdExcept(name));
+    if (!names?.length) return;
+
+    const exceptName = names.find((name) => thisIdExcept(name));
     if (exceptName) {
-      alert('It cannot be done.');
+      alert(`It cannot be done. [ ${exceptName} ]`);
       return;
     }
-    const check = window.confirm(`Delete the [ ${datas.length} ] collections?`);
+    const check = window.confirm(`Delete the [ ${names.length} ] collections?`);
     if (!check) return;
 
-    delsMutating || delDatas(datas, {
+    delsMutating || delDatas(names, {
       onSuccess: () => method.reset(),
     });
   }
@@ -57,7 +54,7 @@ function CollectionBody() {
     <S.Section>
       <FormProvider {...method}>
         <form onSubmit={method.handleSubmit(onDeletes)}>
-          <CollectionControl total={collectLength} checkCount={checkCount} onReset={onReset} />
+          <CollectionControl totalCount={collectLength} checkCount={checkCount} onReset={onReset} />
           <CollectionList isLoading={isLoading} collections={collectData} onDelete={onDelete} />
         </form>
       </FormProvider>
